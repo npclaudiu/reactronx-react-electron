a2c3d009e422ad99313741ea0d200c69dc9dc97d
Claudiu Nedelcu <npclaudiu@users.noreply.github.com>
2026-03-08 00:11:04 +0200

    chore: extend jsx intrinsic elements under react instead of global

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
 export { render } from "./reconciler";
 
-// Add global JSX declarations
-declare global {
-    // eslint-disable-next-line @typescript-eslint/no-namespace
+declare module 'react' {
     namespace JSX {
         interface IntrinsicElements {
             app: Record<string, unknown>;
